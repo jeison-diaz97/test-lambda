8e80dffb687909a15dcc06806b9dfebab5167619
Jeison.Diaz <jeison.diaz@hirehoratio.co>
2025-05-05 09:52:39 -0400

    Changes to code

--- lambda.js
+++ lambda.js
@@ -1,1 +1,7 @@
-print("hello world");
+exports.handler = async (event) => {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify("Hello from Lambda and Github!"),
+    }
+    return response
+  }

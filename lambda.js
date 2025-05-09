01747ad0f14d2921883f7b6ef7924c6a24a55d83
Jeison.Diaz <jeison.diaz@hirehoratio.co>
2025-05-09 11:04:24 -0400

    testing the PR pipeline

--- lambda.js
+++ lambda.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
     const response = {
       statusCode: 200,
-      body: JSON.stringify("Hello from the new deployment yaml"),
+      body: JSON.stringify("Hello from the PR testing pipeline"),
     }
     return response
   }
